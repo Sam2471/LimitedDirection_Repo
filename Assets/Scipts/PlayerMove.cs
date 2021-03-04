5f42a0eb454774eb5b906ed983d9dad66fab505c
Samuel Flowers <475197@chrome.escg.ac.uk>
2021-03-04 20:44:57 +0000

    play

---
+++ Assets/Scipts/PlayerMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-5, 0);
+        }
+                
+              
+    }
+}
