ddb211e290d512124befb351a75f01b5a1033004
Samuel Flowers <475197@chrome.escg.ac.uk>
2021-03-20 22:04:21 +0000

    In game ui done for now.

---
+++ Assets/Scipts/UIControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour
+{
+    public Animator anim2;
+    public Animator anim3;
+    public Animator anim4;
+    public Animator anim5;
+    public Animator anim6;
+    public Animator anim7;
+
+    public bool isfading = false;
+
+    void Start()
+    {
+        StartCoroutine(waitthree());
+        anim2.SetBool("canfade", false);
+        anim3.SetBool("canfade", false);
+        anim4.SetBool("canfade", false);
+        anim5.SetBool("canfade", false);
+        anim6.SetBool("canfade", false);
+        anim7.SetBool("canfade", false);
+    }
+
+    IEnumerator waitthree()
+
+    {
+        yield return new WaitForSeconds(5);
+        anim2.SetBool("canfade", true);
+        anim3.SetBool("canfade", true);
+        anim4.SetBool("canfade", true);
+        anim5.SetBool("canfade", true);
+        anim6.SetBool("canfade", true);
+        anim7.SetBool("canfade", true);
+        isfading = true;
+    }
+}
