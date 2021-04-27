89431f80a8f335276aba6fc1aefb5f7a963ffb03
Sam2471 <samf4553@gmail.com>
2021-04-27 15:59:13 +0100

    need to fin plats

---
+++ Assets/Scipts/Invisplatform.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invisplatform : MonoBehaviour
+{
+    public SpriteRenderer transpancy;
+    private enum State {idle, fadein, fadeout}
+    State state = State.idle;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        state = State.fadeout;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        state = State.fadein;
+    }
+   
+    private void Update()
+    {
+        if (state == State.fadein | state == State.fadeout)
+        {
+
+        }
+        else
+        {
+            state = State.idle;
+        }
+    }
+}
