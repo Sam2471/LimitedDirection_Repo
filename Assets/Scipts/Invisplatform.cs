4e43f59ca38c650684f8a23730cfb8894b1e6f6a
Samuel Flowers <475197@chrome.escg.ac.uk>
2021-04-29 21:55:32 +0100

    Invis plats and background are done

--- Assets/Scipts/Invisplatform.cs
+++ Assets/Scipts/Invisplatform.cs
@@ -4,34 +4,39 @@ using UnityEngine;
 
 public class Invisplatform : MonoBehaviour
 {
-    [SerializeField]private Animator animplat;
+    [SerializeField] private Animator animplat;
     public SpriteRenderer transpancy;
 
-    private enum State {idle, fadeout, fadein}
+    private enum State { idle, fadeout, fadein }
     State state = State.idle;
 
     private void Start()
     {
         animplat = GetComponent<Animator>();
     }
-    
+
     private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log("poop4");
+    {     
         state = State.fadeout;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        Debug.Log("poop5");
         state = State.fadein;
+        waitthree(); 
     }
-   
+
+    IEnumerator waitthree()
+    {
+        state = State.idle;
+        yield return new WaitForSeconds(2);
+    }
+
     private void Update()
     {
         if (state == State.fadein || state == State.fadeout)
         {
-            // bongoes
+            //bongoes
             
         }
         else
