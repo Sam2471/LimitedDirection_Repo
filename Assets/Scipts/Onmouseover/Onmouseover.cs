fe195e1cf8060e437a37254e8068e7bffccbdce9
Samuel Flowers <475197@chrome.escg.ac.uk>
2021-04-30 20:53:37 +0100

    Updated controls and level select, polish up main menu also.

---
+++ Assets/Scipts/Onmouseover/Onmouseover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Onmouseover : MonoBehaviour
+{
+    [SerializeField]private GameObject placeholder;
+
+    private void Start()
+    {
+        placeholder.SetActive(false);
+    }
+
+    public void OnMouseOver()
+    {
+        Debug.Log("poop6");
+        placeholder.SetActive(true);
+    }
+
+    public void OnMouseExit()
+    {
+        placeholder.SetActive(false);
+    }
+}
