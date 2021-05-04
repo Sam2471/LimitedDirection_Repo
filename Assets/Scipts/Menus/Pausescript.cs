41495c85bc8d2f3ace07a21c27387e7b10eda511
Sam2471 <samf4553@gmail.com>
2021-05-04 15:59:18 +0100

    main menu start

---
+++ Assets/Scipts/Menus/Pausescript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pausescript : MonoBehaviour
+{
+    public static bool Ispaused  false;
+    public gameObject Pausemenu;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Ispaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();           
+            }
+        
+        }
+    }
+
+    void Resume() 
+    {
+
+    }
+
+    void Pause()
+    {
+
+    }
+}
