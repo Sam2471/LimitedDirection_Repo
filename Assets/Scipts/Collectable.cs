59933378a44bc96d90a98dac363196ae4f735dd3
Sam2471 <samf4553@gmail.com>
2021-05-05 15:59:59 +0100

    Pause done and webgl made

---
+++ Assets/Scipts/Collectable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public int gemcount = 0;
+
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+       if (collision.tag == ("Player"))
+       {    
+           Destroy(this);
+            gemcount + 1;
+       }
+    }
+}
--- Assets/Scipts/Menus/Pausescript.cs
+++ Assets/Scipts/Menus/Pausescript.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pausescript : MonoBehaviour
 {
-    public static bool Ispaused  false;
-    public gameObject Pausemenu;
-    // Update is called once per frame
+    public static bool Ispaused = false;
+    public GameObject Pausemenu;
+    public GameObject ControlUI;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -18,18 +20,38 @@ public class Pausescript : MonoBehaviour
             else
             {
                 Pause();           
-            }
-        
+            }      
         }
     }
 
-    void Resume() 
+    public void Resume() 
     {
-
+        Pausemenu.SetActive(false);
+        Time.timeScale = 1f;
+        Ispaused = false;
     }
-
+    public void Mainmenu()
+    {
+        SceneManager.LoadScene("Main Menu01");
+    }
+    public void Controls()
+    {        
+        ControlUI.SetActive(true);
+        Pausemenu.SetActive(false);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void Backbutton()
+    {
+        ControlUI.SetActive(false);
+        Pausemenu.SetActive(true);
+    }
     void Pause()
     {
-
+        Pausemenu.SetActive(true);
+        Time.timeScale = 0f;
+        Ispaused = true;
     }
 }
