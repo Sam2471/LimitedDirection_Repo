76e2b183a560fcc41cccc428a7ca1ec4cdbf4af5
Sam2471 <samf4553@gmail.com>
2021-05-14 15:59:28 +0100

    collectable update

--- Assets/Game Assets/Scipts/Onmouseover/Onmouseover.cs
+++ Assets/Game Assets/Scipts/Onmouseover/Onmouseover.cs
@@ -1,27 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Onmouseover : MonoBehaviour
 {
     [SerializeField]private GameObject placeholder;
     public GameObject gemicon;
-    public GameObject gemtext;
+    public GameObject gemtext1;
+    public Text text;
 
     private void Start()
     {
         placeholder.SetActive(false);
+        
     }
 
     public void OnMouseOver()
     {
         placeholder.SetActive(true);
-        gemicon.SetActive(true);      
+        gemicon.SetActive(true);
+        gemtext1.SetActive(true);
+        text.text = PermanentUI.perm.gemsave1.ToString() + "/5";
     }
 
     public void OnMouseExit()
     {
         placeholder.SetActive(false);
         gemicon.SetActive(false);
+        gemtext1.SetActive(false);
     }
 }
--- Assets/Game Assets/Scipts/PermanentUI.cs
+++ Assets/Game Assets/Scipts/PermanentUI.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PermanentUI : MonoBehaviour
 {
-    public int score = 0;
     public int gem = 0;
     public Text gemText;
-    public float gemsave;
+    public float gemsave1;
+    public float gemsave2;
+    public float gemsave3;
 
+    public Scene scene3;
     public static PermanentUI perm;
 
     private void Start()
     {
-        gemsave = PlayerPrefs.GetFloat("gemsave");
+        scene3 = SceneManager.GetActiveScene();
+        
 
         if (!perm)
             perm = this;
@@ -26,7 +30,22 @@ public class PermanentUI : MonoBehaviour
 
     void Update()
     {
-        PlayerPrefs.SetFloat("gemsave", gem);
+        gemsave1 = PlayerPrefs.GetFloat("gemsave1");
+        gemsave2 = PlayerPrefs.GetFloat("gemsave2");
+        gemsave3 = PlayerPrefs.GetFloat("gemsave3");
+        if (scene3.name == "Scene01")
+        {
+            PlayerPrefs.SetFloat("gemsave1", gem);
+        }
+        if (scene3.name == "Scene02")
+        {
+            PlayerPrefs.SetFloat("gemsave2", gem);
+        }
+        
+        if (scene3.name == "Scene03")
+        {
+            PlayerPrefs.SetFloat("gemsave3", gem);
+        }
     }
 
     public void Reset()
