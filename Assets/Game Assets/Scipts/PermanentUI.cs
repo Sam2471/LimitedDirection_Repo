3db62cda951f0c0a86095d939d7bc18d97e63c46
Sam2471 <samf4553@gmail.com>
2021-05-19 10:16:36 +0100

    Merge fix

--- Assets/Game Assets/Scipts/PermanentUI.cs
+++ Assets/Game Assets/Scipts/PermanentUI.cs
@@ -3,25 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PermanentUI : MonoBehaviour
 {
-    public int score = 0;
     public int gem = 0;
     public Text gemText;
-    public float gemsave;
+    public float gemsave1;
+    public float gemsave2;
+    public float gemsave3;
 
+    public Scene scene3;
     public static PermanentUI perm;
 
     private void Start()
     {
-<<<<<<< HEAD
-        gemsave = PlayerPrefs.GetFloat("gemsave");
-
-=======
         scene3 = SceneManager.GetActiveScene();
-        
->>>>>>> 3ca8e09885bebd943d0e12394ffae0d5dc40d53f
+
         if (!perm)
             perm = this;
 
@@ -31,15 +29,31 @@ public class PermanentUI : MonoBehaviour
 
     void Update()
     {
-        PlayerPrefs.SetFloat("gemsave", gem);
+        gemsave1 = PlayerPrefs.GetFloat("gemsave1");
+        gemsave2 = PlayerPrefs.GetFloat("gemsave2");
+        gemsave3 = PlayerPrefs.GetFloat("gemsave3");
+        if (scene3.name == "Scene01")
+        {
+            PlayerPrefs.SetFloat("gemsave1", gem);
+        }
+        if (scene3.name == "Scene02")
+        {
+            PlayerPrefs.SetFloat("gemsave2", gem);
+        }
+
+        if (scene3.name == "Scene03")
+        {
+            PlayerPrefs.SetFloat("gemsave3", gem);
+        }
     }
 
     public void Reset()
     {
         gem = 0;
-        gemText.text = gem.ToString();      
+        gemText.text = gem.ToString();
     }
 
 
 }
+
 
